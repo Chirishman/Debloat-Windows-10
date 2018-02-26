44b3c0b11e7132d733fb9d67bfdbf472fc673ff8
Chirishman <Chirishman@users.noreply.github.com>
2018-02-26 11:55:08 -0500

    fixed typo

--- Win10Bootstrap/Steps/10 - Set-DarkUserInterface.ps1
+++ Win10Bootstrap/Steps/10 - Set-DarkUserInterface.ps1
@@ -3,4 +3,4 @@ do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
 
 write-verbose "Switching to Dark Theme"
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
-Set-ItemProperty -Path "HKLU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
